5041649406d7d81be4e332148bda2e9e48e89ec1
AET Technology <aettechnology5@gmail.com>
2026-06-27 16:26:08 +0200

    Add app/src/main/java/com/aet/togosheets/MainActivity.java

---
+++ app/src/main/java/com/aet/togosheets/MainActivity.java
@@ -0,0 +1,29 @@
+package com.aet.togosheets;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebChromeClient;
+public class MainActivity extends Activity {
+    private WebView webView;
+    private static final String URL = "https://alban3886.github.io/togosheets-pro/public.html";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webview);
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setLoadWithOverviewMode(true);
+        s.setUseWideViewPort(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.loadUrl(URL);
+    }
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) { webView.goBack(); } else { super.onBackPressed(); }
+    }
+}
