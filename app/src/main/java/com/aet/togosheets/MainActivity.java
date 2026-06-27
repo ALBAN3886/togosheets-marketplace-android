4313406b9bc03d0486fcaa0d821281610d698d93
AET Technology <aettechnology5@gmail.com>
2026-06-27 19:19:07 +0200

    Update MainActivity.java

--- app/src/main/java/com/aet/togosheets/MainActivity.java
+++ app/src/main/java/com/aet/togosheets/MainActivity.java
@@ -1,18 +1,27 @@
 package com.aet.togosheets;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.webkit.WebChromeClient;
+import android.widget.RelativeLayout;
+
 public class MainActivity extends Activity {
+
     private WebView webView;
     private static final String URL = "https://alban3886.github.io/togosheets-pro/public.html";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        webView = findViewById(R.id.webview);
+
+        webView = new WebView(this);
+        RelativeLayout layout = new RelativeLayout(this);
+        layout.addView(webView);
+        setContentView(layout);
+
         WebSettings s = webView.getSettings();
         s.setJavaScriptEnabled(true);
         s.setDomStorageEnabled(true);
@@ -22,8 +31,13 @@ public class MainActivity extends Activity {
         webView.setWebChromeClient(new WebChromeClient());
         webView.loadUrl(URL);
     }
+
     @Override
     public void onBackPressed() {
-        if (webView.canGoBack()) { webView.goBack(); } else { super.onBackPressed(); }
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
